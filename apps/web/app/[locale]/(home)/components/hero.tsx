ba7f7afb1d53a238ec3bfcfab267b798b847e7f8
Antonio MB <tono.mtzb@gmail.com>
2025-04-29 14:18:02 -0600

    feat: deletes unused locals and updates home / hero

--- apps/web/app/[locale]/(home)/components/hero.tsx
+++ apps/web/app/[locale]/(home)/components/hero.tsx
@@ -1,9 +1,7 @@
 import { env } from '@/env';
-import { blog } from '@repo/cms';
-import { Feed } from '@repo/cms/components/feed';
 import { Button } from '@repo/design-system/components/ui/button';
 import type { Dictionary } from '@repo/internationalization';
-import { MoveRight, PhoneCall } from 'lucide-react';
+import { Github, MoveRight } from 'lucide-react';
 import Link from 'next/link';
 
 type HeroProps = {
@@ -15,21 +13,16 @@ export const Hero = async ({ dictionary }: HeroProps) => (
     <div className="container mx-auto">
       <div className="flex flex-col items-center justify-center gap-8 py-20 lg:py-40">
         <div>
-          <Feed queries={[blog.latestPostQuery]}>
-            {/* biome-ignore lint/suspicious/useAwait: "Server Actions must be async" */}
-            {async ([data]) => {
-              'use server';
-
-              return (
-                <Button variant="secondary" size="sm" className="gap-4" asChild>
-                  <Link href={`/blog/${data.blog.posts.item?._slug}`}>
-                    {dictionary.web.home.hero.announcement}{' '}
-                    <MoveRight className="h-4 w-4" />
-                  </Link>
-                </Button>
-              );
-            }}
-          </Feed>
+          <Button variant="secondary" size="sm" className="gap-2" asChild>
+            <Link
+              href={dictionary.web.global.github_link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {dictionary.web.home.hero.announcement}
+              <Github className="h-4 w-4" />
+            </Link>
+          </Button>
         </div>
         <div className="flex flex-col gap-4">
           <h1 className="max-w-2xl text-center font-regular text-5xl tracking-tighter md:text-7xl">
@@ -40,14 +33,10 @@ export const Hero = async ({ dictionary }: HeroProps) => (
           </p>
         </div>
         <div className="flex flex-row gap-3">
-          <Button size="lg" className="gap-4" variant="outline" asChild>
-            <Link href="/contact">
-              Get in touch <PhoneCall className="h-4 w-4" />
-            </Link>
-          </Button>
           <Button size="lg" className="gap-4" asChild>
             <Link href={env.NEXT_PUBLIC_APP_URL}>
-              Sign up <MoveRight className="h-4 w-4" />
+              {dictionary.web.global.primaryCta}{' '}
+              <MoveRight className="h-4 w-4" />
             </Link>
           </Button>
         </div>
